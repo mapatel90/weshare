648de0a5c27d8e9764ad83d5e9cca0c8ff48ec2b
Maulik Patel <mapatel90@gmail.com>
2025-11-05 12:33:25 +0530

    design and daynemic data impliment

--- src/app/exchange-hub/page.jsx
+++ src/app/exchange-hub/page.jsx
@@ -5,7 +5,7 @@ import HomeNavbar from '@/components/home/HomeNavbar'
 import HomeFooter from '@/components/home/HomeFooter'
 import ExchangeHub from '@/components/exchange-hub/ExchangeHub'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import '../../../public/css/home.css'
+// import '../../../public/css/home.css'
 import '../../../public/css/responsive.css'
 
 const ExchangeHubPage = () => {
