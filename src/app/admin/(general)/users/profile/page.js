24d11d199f41945a3d37ab3ff510fc16cdbf4819
Maulik Patel <mapatel90@gmail.com>
2025-11-10 15:14:07 +0530

    profile update add

---
+++ src/app/admin/(general)/users/profile/page.js
@@ -0,0 +1,22 @@
+"use client"
+
+import React from 'react'
+import PageHeader from '@/components/shared/pageHeader/PageHeader'
+import ProfilePage from '@/components/users/ProfilePage'
+
+const page = () => {
+    return (
+        <>
+            <PageHeader>
+            {/* <ChangePasswordHeader /> */}
+            </PageHeader>
+            <div className='main-content'>
+                <div className='row'>
+                    <ProfilePage />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default page
