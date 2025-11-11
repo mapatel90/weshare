313bfa4fabfffd23e310e15fa75ec2b3549ceb00
Maulik Patel <mapatel90@gmail.com>
2025-11-11 14:13:31 +0530

    desgin issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import '../../public/css/home.css'
 import '../../public/css/responsive.css'
 import HomePage from './home/page'
