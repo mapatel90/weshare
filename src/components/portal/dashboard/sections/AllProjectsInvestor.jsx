b3582d229b91607c681912c4e3f46d4cc9beb4ee
Maulik Patel <mapatel90@gmail.com>
2026-01-19 11:39:25 +0530

    invester dashboard add month chart and year chart

--- src/components/portal/dashboard/sections/AllProjectsInvestor.jsx
+++ src/components/portal/dashboard/sections/AllProjectsInvestor.jsx
@@ -59,7 +59,7 @@ const AllProjects = ({ title }) => {
   };
 
   return (
-    <div className="col-xxl-4">
+    <div className="col-xxl-6">
       <div
         className={`card stretch shadow stretch-full ${isExpanded ? "card-expand" : ""} ${refreshKey ? "card-loading" : ""}`}
       >
